f6a56771debb8f6c3b0effa9e7fdecabd9fe28eb
Augustin Malle <augustin.malle@gmail.com>
2023-01-24 22:11:32 +0100

    ççasd

---
+++ src/scripts/navigation.js
@@ -0,0 +1,5 @@
+
+document.getElementById('avaxhamburger').addEventListener('click', () => {
+  document.getElementById('avaxhamburger').classList.toggle('active');
+  document.querySelector('#avaxmenu').classList.toggle('hidden');
+});
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,17 @@
 module.exports = {
     mode: 'jit',
     content: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
+    theme: {
+      // ...
+      deliciousHamburgers: {
+        size: '60px', // must be in px.
+        color: '#586061',
+        colorLight: '#fff8f4',
+        padding: '10px', // must be in px.
+        animationSpeed: 2,
+        thickness: '4px'
+      },
+    },
+    variants: {},
+    plugins: [require('tailwindcss-delicious-hamburgers')],
   };
