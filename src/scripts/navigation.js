c4f4e5df3fe0548f2caa11ba51e82c99b550adad
Augustin Malle <augustin.malle@gmail.com>
2023-01-27 09:13:02 +0100

    hg

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -3,4 +3,17 @@ document.getElementById('avaxhamburger').addEventListener('click', () => {
   document.getElementById('avaxhamburger').classList.toggle('active');
   document.getElementById('avaxlogo').classList.toggle('fireanimate');
   document.querySelector('#avaxmenu').classList.toggle('hidden');
+  demo();
+  
 });
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+  await sleep(2000);
+  document.getElementById('avaxlogo').classList.toggle('firefulldoublerainbowbg');
+}
+
