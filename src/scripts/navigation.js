a58c57522ffa309b588911796fd49cbccd9c75a0
Augustin Malle <augustin.malle@gmail.com>
2023-01-28 10:10:20 +0100

    playing around

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -3,7 +3,7 @@ document.getElementById('avaxhamburger').addEventListener('click', () => {
   document.getElementById('avaxhamburger').classList.toggle('active');
   document.getElementById('avaxlogo').classList.toggle('fireanimate');
   document.querySelector('#avaxmenu').classList.toggle('hidden');
-  demo();
+  
   
 });
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,13 +4,23 @@ module.exports = {
     theme: {
       // ...
       deliciousHamburgers: {
-        size: '60px', // must be in px.
-        color: '#586061',
-        colorLight: '#fff8f4',
-        padding: '10px', // must be in px.
+        size: '60px',
+        color: '#f081a2',
+        colorLight: '#ffe7ee',
+        padding: '10px',
         animationSpeed: 2,
         thickness: '4px'
       },
+      colors:{
+        'avaxpink' : '#f081a2',
+        'avaxlightpink' : '#ffe7ee',
+        //'avaxbg' : 'bg-slate-50'
+      },
+      extend:{
+        lineHeight:{
+          '12': '3rem'
+        }
+      }
     },
     variants: {},
     plugins: [require('tailwindcss-delicious-hamburgers')],
