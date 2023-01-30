47fb65dce843244cd9a2213cc83b543a7acd56a7
Augustin Malle <augustin.malle@gmail.com>
2023-01-30 18:07:45 +0100

    skills and animate

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -5,11 +5,19 @@ document.getElementById('avaxhamburger').addEventListener('click', () => {
   
   if(document.getElementById('avaxhamburger').classList.contains('active')){
     document.getElementById('avaxlogo').classList.toggle('wateranimate');
+    
+    document.getElementById('avax-services').classList.remove('avax-blendin');
     document.querySelector('#avaxmenu').classList.add('avax-blendout');  
+    
+    document.querySelector('#avax-services').classList.add('avax-blendin');  
     document.querySelector('#avaxmaintitle').classList.toggle('avax-anim-neg');
     document.querySelector('#avaxmaintitle').classList.remove('avax-anim');
   }else{
     document.getElementById('avaxmenu').classList.remove('avax-anim-neg');
+
+    document.getElementById('avax-services').classList.remove('avax-blendin');
+    document.querySelector('#avax-services').classList.add('avax-blendout');
+    
     document.querySelector('#avaxmaintitle').classList.toggle('avax-anim');
     document.querySelector('#avaxmaintitle').classList.remove('avax-anim-neg');
   }
@@ -19,8 +27,6 @@ document.getElementById('avaxhamburger').addEventListener('click', () => {
   document.querySelector('#avaxmenu').classList.toggle('avax-blendin');
   //document.querySelector('#avaxmenu').classList.toggle('avax-animate');
   //document.querySelector('#avaxmaintitle').classList.toggle('hidden');
-  
-
   
 });
 
